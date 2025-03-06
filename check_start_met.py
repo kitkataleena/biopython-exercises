5b182394999874224b4422e0efebbda644c1ca67
Kataleena Huynh <kataleenahuynh02@gmail.com>
2025-03-06 16:40:18 -0600

    Deleted extra python scripts

--- check_start_met.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-filename = "PGSC_DM_v3.4_pep_representative.fasta"
-bad = 0
-for record in SeqIO.parse(filename, "fasta"):
-	if not record.seq.startswith("M"):
-		bad += 1
-		print(f"{record.id} starts with {record.seq[0]}")
-print(f"Found {str(bad)} records in {filename} which did not start with M")
--- record_lengths.py
+++
@@ -1,5 +0,0 @@
-from Bio import SeqIO
-filename = "NC_000913.faa"
-for record in SeqIO.parse(filename, "fasta"):
-	print(f"Record {record.id}, length {str(len(record.seq))}")
-
